c05f6253f4b8cf3228c6c952c2fe3e9373fd1c9f
AlejandroXVII <m.alejandro.xvii@gmail.com>
2024-02-04 03:31:19 -0400

    Update about page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,10 +1,12 @@
 const AboutPage = () => {
 	return (
-		<div className="AboutContainer">
-			<h1>HERE IS THE ABOUT PAGE</h1>
-			<h1>HERE IS THE ABOUT PAGE</h1>
-			<h1>HERE IS THE ABOUT PAGE</h1>
-			<h1>HERE IS THE ABOUT PAGE</h1>
+		<div className="AboutContainer error">
+			<h1>Hay, there!</h1>
+			<h2>I'm Manuel Hernandez, web Developer</h2>
+			<p>
+				If you are interesting on see what I am working you can visit my{" "}
+				<a href="https://github.com/AlejandroXVII">github</a> account
+			</p>
 		</div>
 	);
 };
